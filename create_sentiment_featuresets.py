99bd0deabcae65a822a61f1ae42485bc16d5783a
root <root@localhost.localdomain>
2018-12-11 18:28:06 +0300

    create sentiment feature set half of it

--- create_sentiment_featuresets.py
+++ create_sentiment_featuresets.py
@@ -8,5 +8,59 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import pickle
+from collections import Counter
 
+lemmatizer = WordNetLemmatizer()
+hm_lines = 10000000
+
+# MemoryError -> we may need lots of ram and server
+def create_lexicon(pos,neg):
+    lexicon = []
+    for fi in [pos,neg]:
+        with open(fi,'r') as f:
+            contents = f.readlines()
+            for l in contents[:hm_lines]:
+                all_words = word_tokenize(l)
+                lexicon += list(all_words)
+
+    lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+    w_counts = Counter(lexicon)
+    # w_counts = {'the':51235,'and':2555} somethink like this
+
+    l2 = []
+    for w in w_counts:
+        if 5000 > w_counts[w] > 50:
+            l2.append(w)
+
+    return l2
+
+def sample_handling(sample, lexicon, classification):
+    featureset = []
+
+    with open(sample,'r') as f:
+        contents = f.readlines()
+        for l in contents[:hm_lines]:
+            current_words = word_tokenize(l.lower())
+            current_words = [lemmatizer.lemmatize(i) for i in current_words]
+            features = np.zeros(len(lexicon))
+            for word in current_words:
+                if word.lower() in lexicon:
+                    index_value = lexicon.index_value(word.lower())
+                    features[index_value] += 1
+            features = list(features)
+            featureset.append([features, classification])
+
+    return featureset
+
+def create_feature_sets_and_labels(pos,neg,test_size=0.1):
+    lexicon = create_lexicon(pos,neg)
+    features = []
+    features += sample_handling('pos.txt',lexicon,[1,0])
+    features += sample_handling('neg.txt',lexicon,[0,1])
+    random.shuffle(features)
+
+    # does tf.argmax([output]) == tf.argmax([expectations])
 
