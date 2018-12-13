0a7930db1f1d2490f427fc490e30f7a493c4e8ee
root <root@localhost.localdomain>
2018-12-13 09:28:52 +0300

    bigdata test

--- create_sentiment_featuresets.py
+++ create_sentiment_featuresets.py
@@ -35,6 +35,7 @@ def create_lexicon(pos,neg):
         if 5000 > w_counts[w] > 50:
             l2.append(w)
 
+    print(len(l2))
     return l2
 
 def sample_handling(sample, lexicon, classification):
@@ -48,7 +49,7 @@ def sample_handling(sample, lexicon, classification):
             features = np.zeros(len(lexicon))
             for word in current_words:
                 if word.lower() in lexicon:
-                    index_value = lexicon.index_value(word.lower())
+                    index_value = lexicon.index(word.lower())
                     features[index_value] += 1
             features = list(features)
             featureset.append([features, classification])
@@ -63,4 +64,24 @@ def create_feature_sets_and_labels(pos,neg,test_size=0.1):
     random.shuffle(features)
 
     # does tf.argmax([output]) == tf.argmax([expectations])
+    # tf.argmax([52412,23412]) == tf.argmax([1,0])
+    # if shuffle the data
+    # tf.argmax([99999,-99999]) == tf.argmax([1,0])
+
+    features = np.array(features)
+
+    test_size = int(test_size*len(features))
+
+    train_x = list(features[:,0][:-test_size])
+    train_y = list(features[:,1][:-test_size])
+
+    test_x = list(features[:,0][-test_size:])
+    test_y = list(features[:,1][-test_size:])
+
+    return train_x,train_y,test_x,test_y
+
+if __name__ == '__main__':
+    train_x,train_y,test_x,test_y = create_feature_sets_and_labels('pos.txt','neg.txt')
+    with open ('sentiment_set.pickle','wb') as f:
+        pickle.dump([train_x,train_y,test_x,test_y],f)
 
