4af39890784c086fc630701e585535ec737cd4ce
root <root@localhost.localdomain>
2018-12-20 09:36:05 +0300

    rnn_example

--- MeanShiftBlobs.py
+++ MeanShiftBlobs.py
@@ -1,6 +1,6 @@
 from sklearn.datasets.samples_generator import make_blobs
 import sklearn.cluster as clstr
-import MeanShiftBlobs
+# import MeanShiftBlobs
 import matplotlib.pyplot as plt
 import matplotlib.figure
 from mpl_toolkits.mplot3d import Axes3D
