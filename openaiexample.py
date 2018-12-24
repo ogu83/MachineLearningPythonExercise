aad27b2c6035d12a89204202116866ab1a717e4f
root <root@localhost.localdomain>
2018-12-24 10:17:26 +0300

    cnn_example + openai gym game player

---
+++ openaiexample.py
@@ -0,0 +1,71 @@
+import gym
+import random
+import numpy as np 
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import mean, median
+from collections import Counter
+
+LR = 1e-3
+env = gym.make('CartPole-v0')
+env.reset()
+goal_steps = 500
+score_reqirement = 50
+initial_games = 10000
+
+def some_random_games_first():
+    for episode in range(5):
+        env.reset()
+        for t in range(goal_steps):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
+
+# some_random_games_first()
+
+def initial_population():
+    training_data = []
+    scores = []
+    accepted_scores = []
+    for _ in range(initial_games):
+        score = 0
+        game_memory = []
+        prev_observation = []
+        for _ in range(goal_steps):
+            action = random.randrange(0,2)
+            observation, reward, done, info = env.step(action)
+
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+
+            prev_observation = observation
+            score += reward
+            if done:
+                break
+
+        if score >= score_reqirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
+
+                training_data.append([data[0], output])
+        
+        env.reset()
+        scores.append(score)
+
+    training_data_save = np.array(training_data)
+    np.save('saved.npy', training_data_save)
+
+    print('Average accepted score:', mean(accepted_scores))
+    print('Median sccepted score:', median(accepted_scores))
+    print(Counter(accepted_scores))
+
+    return training_data
+
+initial_population()
