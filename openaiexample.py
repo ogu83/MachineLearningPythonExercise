01de96b04967cf0920accf3076cbc650af37a2d1
root <root@localhost.localdomain>
2018-12-25 09:40:32 +0300

    open ai example

--- openaiexample.py
+++ openaiexample.py
@@ -6,6 +6,7 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from statistics import mean, median
 from collections import Counter
+import pickle
 
 LR = 1e-3
 env = gym.make('CartPole-v0')
@@ -68,4 +69,47 @@ def initial_population():
 
     return training_data
 
-initial_population()
+def neural_network_model(input_size):
+    network = input_data(shape=[None, input_size, 1], name='input')
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 512, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(network, tensorboard_dir='log')
+
+    return model
+
+def train_model(training_data, model=False):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]),1)
+    y = [i[1] for i in training_data]
+
+    if not model:
+        model = neural_network_model(input_size = len(X[0]))
+
+    model.fit({'input':X}, {'targets':y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openaistuff')
+
+    return model
+
+training_data = initial_population()
+model = train_model(training_data)
+
+# with open('openai_model.pickle','wb') as f:
+#     pickle.dump(model, f)
+
+# pickle_in = open('openai_model.pickle','rb')
+# model = pickle.load(pickle_in)
