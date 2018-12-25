767e58193b8a6254e8d7dcb1cc52e2370264f8e6
root <root@localhost.localdomain>
2018-12-25 18:36:23 +0300

    CartPole_10_Games

--- openaiexample.py
+++ openaiexample.py
@@ -107,9 +107,35 @@ def train_model(training_data, model=False):
 
 training_data = initial_population()
 model = train_model(training_data)
+model.save('CartPole.model')
 
-# with open('openai_model.pickle','wb') as f:
-#     pickle.dump(model, f)
+# model = tflearn.DNN(network, tensorboard_dir='log')
+# model.load('CartPole.model')
 
-# pickle_in = open('openai_model.pickle','rb')
-# model = pickle.load(pickle_in)
+scores = []
+choices = []
+for each_game in range(10):
+    score = 0
+    game_memory = []
+    prev_obs = []
+    env.reset()
+    for _ in range(goal_steps):
+        env.render()
+        if len(prev_obs) == 0:
+            action = random.randrange(0,2)
+        else:
+            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs),1))[0])
+        choices.append(action)
+
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        game_memory.append([new_observation, action])
+        score += reward
+        if done:
+            break
+        
+    scores.append(score)
+
+print('Average Score', sum(scores)/len(scores))
+
+print('Choice 1: {}, Choice 2: {}'.format(choices.count(1)/len(choices), choices.count(0)/len(choices)))
