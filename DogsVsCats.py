6e0df8fdae4d8d91e48f7bb355c5a42d0b535bfe
root <root@localhost.localdomain>
2018-12-27 15:13:48 +0300

    DogsVsCats Submission

--- DogsVsCats.py
+++ DogsVsCats.py
@@ -9,6 +9,8 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
 tf.reset_default_graph()
 
@@ -17,7 +19,7 @@ TEST_DIR = '/media/sf_F_DRIVE/Kaggle/CatsVsDogs/test'
 IMG_SIZE = 50
 LR = 1e-3
 
-MODEL_NAME = "dogvscats-{}-{}.model".format(LR,'2conv-basic')
+MODEL_NAME = "dogvscats-{}-{}.model".format(LR,'6conv-basic')
 
 def label_img(img):
     # dog.93.png
@@ -38,11 +40,11 @@ def create_train_data():
 
 def process_test_data():
     testing_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        path = os.path.join(TRAIN_DIR,img)
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR,img)
         img_num = img.split('.')[0]
         img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMG_SIZE, IMG_SIZE))
-        training_data.append([np.array(img), img_num])        
+        testing_data.append([np.array(img), img_num])        
     np.save('test_data.npy', testing_data)
     return testing_data
 
@@ -79,7 +81,6 @@ convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categori
 
 model = tflearn.DNN(convnet, tensorboard_dir='log')
 
-
 if os.path.exists('{}.meta'.format(MODEL_NAME)):
     model.load(MODEL_NAME)
     print('model loaded')
@@ -95,8 +96,50 @@ Y = [i[1] for i in train]
 test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 test_y = [i[1] for i in test]
 
-model.fit({'input':X}, {'targets':Y}, n_epoch=5, 
-    validation_set=({'input':test_x}, {'targets':test_y}),
-    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+# model.fit({'input':X}, {'targets':Y}, n_epoch=5, 
+#     validation_set=({'input':test_x}, {'targets':test_y}),
+#     snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+# model.save(MODEL_NAME)
 
-model.save(MODEL_NAME)
+model.load(MODEL_NAME)
+
+# test_data = process_test_data()
+#if you already have train data:
+test_data = np.load('test_data.npy')
+
+fig = plt.figure()
+
+for num, data in enumerate(test_data[:12]):
+    #cat : [1,0]
+    #dog : [0,1]
+
+    img_num = data[1]
+    img_data = data[0]
+
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+
+    model_out = model.predict([data])[0]
+
+    if np.argmax(model_out) == 1: str_label = 'Dog'
+    else: str_label = 'Cat'
+
+    y.imshow(orig, cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+
+plt.show()
+
+with open('submission-file.csv','w') as f:
+    f.write('id,label\n')
+
+with open('submission-file.csv','a') as f:
+    for data in tqdm(test_data):
+        img_num = data[1]
+        img_data = data[0]        
+        orig = img_data
+        data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+        model_out = model.predict([data])[0]
+        f.write('{},{}\n'.format(img_num, model_out[1]))
